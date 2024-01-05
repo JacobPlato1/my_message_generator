6f4be272182dcfd12b23e0da67a26f979539ac12
Jacob Plato <jacke.plato@gmail.com>
2024-01-05 09:48:08 +0100

    add instructions

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,1 +1,12 @@
+/*
+ Every time a user runs a program, they should get a new, randomized output.
+
+ To make your program truly random, 
+ the message that it outputs should be made up of at least three different pieces of data.
+
+ 
+*/
+
+
+
 console.log("hello");
